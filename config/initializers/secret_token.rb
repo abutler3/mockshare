c3b5a586073edb9ee7ae00a2c8ecf79589ca3982
Drew Butler <abutler3@gmail.com>
2013-01-30 11:02:12 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mockshare::Application.config.secret_token = '05c7a8e14c9bf0b591d36f66880982e2e11de8cf09265500349b1c8a9e3c2ad964fa3aff217a41809651e5ed49fc499f523d2560493d0f6c81ef099a9e354ee0'

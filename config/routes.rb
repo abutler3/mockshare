f3894e9dab56e65326c53bdf8df7c7aea64fc8a7
Drew Butler <abutler3@gmail.com>
2013-01-31 14:30:45 -0600

    Create partials for home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Mockshare::Application.routes.draw do
+  get 'about' => "pages#about"
+
+  root :to => 'pages#home'
+  # get "pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

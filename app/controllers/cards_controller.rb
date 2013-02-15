5dc8ca6a6906fdf191bce5de2b9bed9b0299e442
Drew Butler <abutler3@gmail.com>
2013-02-14 20:03:23 -0600

    Styling cards and order cards starting with newest

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,7 +4,7 @@ class CardsController < ApplicationController
   # GET /cards
   # GET /cards.json
   def index
-    @cards = Card.all
+    @cards = Card.order("created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
